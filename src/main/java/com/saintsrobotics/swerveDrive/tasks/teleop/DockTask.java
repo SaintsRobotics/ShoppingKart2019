f60341c87376189e2421b1d3b6c6fb95923460ab
thetaspirit <delaynieym@outlook.com>
2019-01-24 18:48:47 -0800

    Changed dock task to runEachFrame

--- src/main/java/com/saintsrobotics/swerveDrive/tasks/teleop/DockTask.java
+++ src/main/java/com/saintsrobotics/swerveDrive/tasks/teleop/DockTask.java
@@ -1,25 +1,44 @@
 package com.saintsrobotics.swerveDrive.tasks.teleop;
 
-import com.github.dozer.coroutine.helpers.RunContinuousTask;
+import java.sql.Driver;
+
+import com.github.dozer.coroutine.helpers.RunEachFrameTask;
 import com.saintsrobotics.swerveDrive.Robot;
 
 import org.opencv.core.Rect;
 
-public class DockTask extends RunContinuousTask{
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DockTask extends RunEachFrameTask{
 
     private Rect targetOne;
     private Rect targetTwo;
-    private Object imgLock = new Object();
+    private Object imgLock;
+    private SwerveControl sc;
 
     public DockTask(Rect targetOne, Rect targetTwo, Object imgLock) {
         this.targetOne = targetOne;
         this.targetTwo = targetTwo;
         this.imgLock = imgLock;
+        //this.sc = sc;
     }
 
-    protected void runForever() {
-        wait.until(() -> Robot.instance.oi.xboxInput.START());
+    @Override
+    protected void runEachFrame() {
+        synchronized (imgLock) {
+            if (targetOne == null || targetTwo == null) {
+                DriverStation.reportWarning("No rectangles", false);
+                return;
+            }
+            DriverStation.reportWarning("Yes rectangles", false);
+            Double t1 = targetOne.tl().x;
+            Double t2 = targetTwo.br().x;
+            SmartDashboard.putNumber("Target 1", targetOne.tl().x);
+            SmartDashboard.putNumber("Target 2", targetTwo.br().x);
+            SmartDashboard.putNumber("Center", t1 + (t2-t1) );
 
-        //write PID loop here
+            //write PID loop here
+        }
     }
 }
