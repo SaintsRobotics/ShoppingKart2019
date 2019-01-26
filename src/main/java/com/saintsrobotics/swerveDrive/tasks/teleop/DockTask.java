2bf8e68dbd9ac27f75a95822af53ebf8242da87b
thetaspirit <delaynieym@outlook.com>
2019-01-26 11:39:07 -0800

    initialized pid controller, sender, and reciever in DockTask
    Added class PidSender

--- src/main/java/com/saintsrobotics/swerveDrive/tasks/teleop/DockTask.java
+++ src/main/java/com/saintsrobotics/swerveDrive/tasks/teleop/DockTask.java
@@ -5,20 +5,38 @@ import java.sql.Driver;
 import com.github.dozer.coroutine.helpers.RunEachFrameTask;
 import com.saintsrobotics.swerveDrive.Robot;
 import com.saintsrobotics.swerveDrive.util.VisionBroker;
-
 import org.opencv.core.Rect;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
+import com.saintsrobotics.swerveDrive.util.PIDReceiver;
+import com.saintsrobotics.swerveDrive.util.PidSender;
 
 public class DockTask extends RunEachFrameTask{
 
     private VisionBroker broker;
+
+    private PIDReceiver pidReciever;
+    private PidSender pidSender;
+    private PIDController pidController;
+
     private SwerveControl sc;
 
     public DockTask(VisionBroker broker, SwerveControl sc) {
         this.broker = broker;
         this.sc = sc;
+
+        this.pidReciever = new PIDReceiver();
+        this.pidSender = new PidSender();
+        this.pidController = new PIDController(0.0, 0.0, 0.0,
+            this.pidSender, pidReciever);
+        this.pidController.setAbsoluteTolerance(2.0);
+        this.pidController.setOutputRange(-1, 1);
+        this.pidController.setInputRange(0, 360);
+        this.pidController.setContinuous();
+        this.pidController.reset();
+        this.pidController.enable();
     }
 
     @Override
@@ -37,6 +55,7 @@ public class DockTask extends RunEachFrameTask{
         SmartDashboard.putNumber("Center", t1 + (t2-t1) );
         
         //write PID loop here
-        
+        this.pidSender.setValue((t2+t1)/2);
+        this.pidController.setSetpoint(180); //any way to bring this value up a few abstractions?
     }
 }
---
+++ src/main/java/com/saintsrobotics/swerveDrive/util/PidSender.java
@@ -0,0 +1,30 @@
+package com.saintsrobotics.swerveDrive.util;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+public class PidSender implements PIDSource {
+
+    private PIDSourceType pidSourceType = PIDSourceType.kDisplacement;
+    private double value = 0;
+
+    @Override
+    public PIDSourceType getPIDSourceType() {
+        return this.pidSourceType;
+    }
+
+    @Override
+    public double pidGet() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public void setPIDSourceType(PIDSourceType pidSource) {
+        this.pidSourceType = pidSource;
+    }
+
+}
